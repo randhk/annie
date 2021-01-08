471467ed98ea40bfaa44575efc8809d7f18c4bf3
randhk <63386886+randhk@users.noreply.github.com>
2021-01-08 18:41:13 +0900

    Add files via upload

---
+++ js/default.js
@@ -0,0 +1,5 @@
+         jQuery(document).ready(function () {
+            $("#head").load("https://diverse-webstatic-files.s3.ap-northeast-2.amazonaws.com/cdn/hiswill/asset/header.html");
+            $("#foot").load("https://diverse-webstatic-files.s3.ap-northeast-2.amazonaws.com/cdn/hiswill/asset/footer.html");
+
+          });
