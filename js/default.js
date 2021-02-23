211ab20a46bbeb153a6ede5edad18611c72b4cb8
randhk <63386886+randhk@users.noreply.github.com>
2021-02-23 23:18:24 +0900

    Add files via upload

--- js/default.js
+++ js/default.js
@@ -4,11 +4,11 @@ jQuery(document).ready(function() {
         $(".hireme").load("https://randhk.github.io/annie/hireMe.html");
         $("#foot").load("https://randhk.github.io/annie/footer.html");
 
-	    var swiper = new Swiper('.swiper-container', {
-	      pagination: {
-	        el: '.swiper-pagination',
-	      },
-	    });
+	    // var swiper = new Swiper('.swiper-container', {
+	    //   pagination: {
+	    //     el: '.swiper-pagination',
+	    //   },
+	    // });
 	    
         $('.tabs-wrap > ul.tabs-top > li > a').on('click', function (event) {
         event.preventDefault();
