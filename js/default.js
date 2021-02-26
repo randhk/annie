f592426c84e020a33cb1b2fa4b59a0b9232fc06d
randhk <63386886+randhk@users.noreply.github.com>
2021-02-26 13:21:28 +0900

    Add files via upload

--- js/default.js
+++ js/default.js
@@ -1,3 +1,8 @@
+
+
+$(window).load(function() { 
+        $('.loading').fadeOut(1000); 
+});
 jQuery(document).ready(function() {
         $("#head").load("https://randhk.github.io/annie/header.html");
         $(".head-sub").load("https://randhk.github.io/annie/header_sub.html");
@@ -18,5 +23,18 @@ jQuery(document).ready(function() {
         $('.tabs-wrap > ul.tabs-top > li').removeClass('active');
         $(this).closest('.tabs-wrap').find('.tabs-con').removeClass('show');
         $(this).closest('li').addClass('active');
+
+        var subPage = new Array;
+        subPage[0] = "sub";
+        subPage[1] = "sub";
+        var url = location.href;
+        var getAr0 = url.indexOf(subPage[0]);
+        var getAr1 = url.indexOf(subPage[1]);
+        if(getAr0 != -1){
+            $(".menu a:eq(1)").addClass("active")
+        };
+        if(getAr1 != -1){
+            $(".menu a:eq(2)").addClass("active")
+        };       
     });
 });
