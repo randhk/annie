7b0a57b96dd194aa91c0c073df03171b330e8efe
randhk <63386886+randhk@users.noreply.github.com>
2021-02-26 14:09:43 +0900

    Add files via upload

--- js/default.js
+++ js/default.js
@@ -23,6 +23,24 @@ jQuery(document).ready(function() {
         $('.tabs-wrap > ul.tabs-top > li').removeClass('active');
         $(this).closest('.tabs-wrap').find('.tabs-con').removeClass('show');
         $(this).closest('li').addClass('active');
-      
+
     });
+
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');        
 });
